d01d6120f935d41d89d3b07ccce320b651e125ac
krsherrill <kirk_Sherrill@nps.gov>
2024-08-13 16:17:40 -0600

    Update ETL_SNPLPORE.py
    Creating skeleton methods for ETL workflow

--- ETL_SNPLPORE.py
+++ ETL_SNPLPORE.py
@@ -17,8 +17,9 @@ logger = logging.getLogger(__name__)
 
 class etl_SNPLPORE:
     def __init__(self):
+
         """
-        Define the instantiated QC Protocol instantiation attributes
+        Define the QC Protocol instantiation attributes
 
         :param TBD
         :return: zzzz
@@ -33,6 +34,7 @@ class etl_SNPLPORE:
         numETL_SNPLPORE += 1
 
     def process_ETLSNPLPORE(outDFDic, etlInstance, dmInstance):
+
         """
         Import files in passed folder to dataframe(s). Uses GLOB to get all files in the directory.
         Currently defined to import .csv, and .xlsx files
@@ -45,6 +47,29 @@ class etl_SNPLPORE:
         """
 
         try:
+
+
+            ######
+            # Process Survey Metadata Form
+            ######
+            outDFSurvey = etl_SNPLPORE.proces_Survey(outDFDic, etlInstance, dmInstance)
+
+            ######
+            # Process SNPL Observations Form
+            ######
+            outDFObs = etl_SNPLPORE.proces_Observations(outDFDic, etlInstance, dmInstance, outDFSurvey)
+
+            ######
+            # Process Bands Sub Form
+            ######
+            outDFBands = etl_SNPLPORE.proces_Bands(outDFDic, etlInstance, dmInstance, outDFSurvey, outDFObs)
+
+            ######
+            # Process Predator
+            ######
+            outDFPredator = etl_SNPLPORE.proces_Predator(outDFDic, etlInstance, dmInstance, outDFSurvey)
+
+
 
             # Pass Variable Success
             outETL = 'Success'
@@ -64,27 +89,36 @@ class etl_SNPLPORE:
 
     def proces_Survey(outDFDic, etlInstance, dmInstance):
 
-            """
-            ETL routine for the parent survey form SFAN_SNPLPORE_Survey{YearVersion}- table.
-            The majority of this information on this form will be pushed to the following tables:
-            tblEvents
-            xxxx
+        """
+        ETL routine for the parent survey form SFAN_SNPLPORE_Survey{YearVersion}- table.
+        The majority of this information on this form will be pushed to the following tables:
+        tblEvents
 
-            :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
-            :param etlInstance: ETL processing instance
-            :param dmInstance: Data Management instance:
+        :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+        :param etlInstance: ETL processing instance
+        :param dmInstance: Data Management instance:
 
-            :return:outDFSurvey: Data Frame of the exported form will be used in subsequent table ETL.
-            """
+        :return:outDFSurvey: Data Frame of the exported form will be used in subsequent table ETL.
+        """
 
         try:
+            #Export the Survey Dataframe for Directionary list
+            inDF = outDFDic[0]
 
-            # Pass Output Data Frame
-            outDFSurvey = []
 
-            logMsg = f"Success ETL_SNPLPORE.py - proces_Survey."
+
+            #Define final query
+            insertQuery = f""
+            # Pass final Query to be appended
+            cnxn = dm.generalDMClass.connect_DB_Access(etlInstance.inDBBE)
+            outAppend = dm.generalDMClass.appendDataSet(cnxn, dfToAppend, "tblEvents", insertQuery,
+                                                        dmInstance)
+
+            logMsg = f"Success ETL_SNPLPORE.py - process_Survey"
             dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
             logging.info(logMsg)
+
+            #Might Return a DF with a query of tblEvents aftern appending - TBD
 
             return outDFSurvey
 
@@ -95,71 +129,80 @@ class etl_SNPLPORE:
             logging.critical(logMsg, exc_info=True)
             traceback.print_exc(file=sys.stdout)
 
-        def proces_Observations(outDFDic, etlInstance, dmInstance, outDFSurvey):
-            try:
-                """
-                ETL routine for the 'SNPL Observation' form - table 'SNPL_Observations'.
-                The major of information on this form is pushed to the following tables:
-                tbl_SNPL_Observations
-                xxxx
+    def proces_Observations(outDFDic, etlInstance, dmInstance, outDFSurvey):
 
-                :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
-                :param etlInstance: ETL processing instance
-                :param dmInstance: Data Management instance
-                :param outDFSurvey: Data frame output from the proces_Survey method.  Used is workflow processing.
+        """
+        ETL routine for the 'SNPL Observation' form - table 'SNPL_Observations'.
+        The major of information on this form is pushed to the following tables:
+        tbl_SNPL_Observations
+        xxxx
 
-                :return:outDFObs: Data Frame with the exported/imported Observation data, to be used as needed.  
-                """
+        :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+        :param etlInstance: ETL processing instance
+        :param dmInstance: Data Management instance
+        :param outDFSurvey: Data frame output from the proces_Survey method.  Used is workflow processing.
 
-                # Pass Output Data Frame
-                outDFSurvey = []
+        :return:outDFObs: Data Frame with the exported/imported Observation data, to be used as needed.
+        """
 
-                logMsg = f"Success ETL_SNPLPORE.py - process_Observations."
-                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
-                logging.info(logMsg)
+        try:
 
-                return outDFObs
-
-            except Exception as e:
-
-                logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_Observations: {e}'
-                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
-                logging.critical(logMsg, exc_info=True)
-                traceback.print_exc(file=sys.stdout)
-
-        def proces_Bands(outDFDic, etlInstance, dmInstance, outDFSurvey, outDFObs):
-
-            """
-            ETL routine for the 'SNPL Bands Sub Form' form - table 'tbl_SNPL_Banded'.
-            The major of information on this form is pushed to the following tables:
-            tbl_SNPL_Banded
+            # Define final query
+            insertQuery = f""
+            # Pass final Query to be appended
+            cnxn = dm.generalDMClass.connect_DB_Access(etlInstance.inDBBE)
+            outAppend = dm.generalDMClass.appendDataSet(cnxn, dfToAppend, "tblEvents", insertQuery,
+                                                        dmInstance)
 
 
-            :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
-            :param etlInstance: ETL processing instance
-            :param dmInstance: Data Management instance
-            :param outDFSurvey: Data frame output from the proces_Survey method.
-            :param outDFObs: Data frame output from the process_Observations method.
+            logMsg = f"Success ETL_SNPLPORE.py - process_Observations."
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.info(logMsg)
 
-            :return:outDFBands: Data Frame with the exported/imported data, to be used as needed.
-            """
+            # Might Return a DF with a query of tblEvents aftern appending - TBD
 
-            try:
-                # Pass Output Data Frame
-                outDFSurvey = []
+            return outDFObs
 
-                logMsg = f"Success ETL_SNPLPORE.py - process_Bands."
-                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
-                logging.info(logMsg)
+        except Exception as e:
 
-                return outDFBands
+            logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_Observations: {e}'
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.critical(logMsg, exc_info=True)
+            traceback.print_exc(file=sys.stdout)
 
-            except Exception as e:
+    def proces_Bands(outDFDic, etlInstance, dmInstance, outDFSurvey, outDFObs):
 
-                logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_Bands: {e}'
-                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
-                logging.critical(logMsg, exc_info=True)
-                traceback.print_exc(file=sys.stdout)
+        """
+        ETL routine for the 'SNPL Bands Sub Form' form - table 'tbl_SNPL_Banded'.
+        The major of information on this form is pushed to the following tables:
+        tbl_SNPL_Banded
+
+
+        :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+        :param etlInstance: ETL processing instance
+        :param dmInstance: Data Management instance
+        :param outDFSurvey: Data frame output from the proces_Survey method.
+        :param outDFObs: Data frame output from the process_Observations method.
+
+        :return:outDFBands: Data Frame with the exported/imported data, to be used as needed.
+        """
+
+        try:
+            # Pass Output Data Frame
+            outDFSurvey = []
+
+            logMsg = f"Success ETL_SNPLPORE.py - process_Bands."
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.info(logMsg)
+
+            return outDFBands
+
+        except Exception as e:
+
+            logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_Bands: {e}'
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.critical(logMsg, exc_info=True)
+            traceback.print_exc(file=sys.stdout)
 
     def proces_Predator(outDFDic, etlInstance, dmInstance, outDFSurvey):
 
