7e64eab4b2a68aced13baddc713592a9249c27df
krsherrill <kirk_Sherrill@nps.gov>
2024-08-13 11:09:39 -0600

    Update ETL_SNPLPORE.py
    Adding method - process_ETLSNPLPORE where ETL will be done.

--- ETL_SNPLPORE.py
+++ ETL_SNPLPORE.py
@@ -11,6 +11,7 @@ import traceback
 import ETL as ETL
 import generalDM as dm
 import logging
+import log_config
 
 logger = logging.getLogger(__name__)
 
@@ -30,3 +31,35 @@ class etl_SNPLPORE:
         #self.filterRecQuery = 'qsel_QA_Control'
 
         numETL_SNPLPORE += 1
+
+    def process_ETLSNPLPORE(outDFDic, etlInstance, dmInstance):
+        """
+        Import files in passed folder to dataframe(s). Uses GLOB to get all files in the directory.
+        Currently defined to import .csv, and .xlsx files
+
+        :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+        :param etlInstance: ETL processing instance
+        :param dmInstance: Data Management instance:
+
+        :return:outETL: String denoting 'Success' or 'Error' on ETL Processing
+        """
+
+        try:
+
+            
+
+            # Pass Variable Success
+            outETL = 'Success'
+
+            logMsg = f"Success ETL_SNPLPORE.py - process_ETLSNPLPORE."
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.info(logMsg)
+
+            return outETL
+
+        except Exception as e:
+
+            logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_ETLSNPLPORE: {e}'
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.critical(logMsg, exc_info=True)
+            traceback.print_exc(file=sys.stdout)
