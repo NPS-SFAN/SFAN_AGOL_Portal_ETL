b221e4246a7cbce89660d7108cef1b4e01bb457d
krsherrill <kirk_Sherrill@nps.gov>
2024-08-13 14:05:02 -0600

    Update ETL_SNPLPORE.py
    Skeltons for methods - Survey, Observations, Bands and Predators added.

--- ETL_SNPLPORE.py
+++ ETL_SNPLPORE.py
@@ -46,8 +46,6 @@ class etl_SNPLPORE:
 
         try:
 
-            
-
             # Pass Variable Success
             outETL = 'Success'
 
@@ -60,6 +58,138 @@ class etl_SNPLPORE:
         except Exception as e:
 
             logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_ETLSNPLPORE: {e}'
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.critical(logMsg, exc_info=True)
+            traceback.print_exc(file=sys.stdout)
+
+    def proces_Survey(outDFDic, etlInstance, dmInstance):
+
+            """
+            ETL routine for the parent survey form SFAN_SNPLPORE_Survey{YearVersion}- table.
+            The majority of this information on this form will be pushed to the following tables:
+            tblEvents
+            xxxx
+
+            :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+            :param etlInstance: ETL processing instance
+            :param dmInstance: Data Management instance:
+
+            :return:outDFSurvey: Data Frame of the exported form will be used in subsequent table ETL.
+            """
+
+        try:
+
+            # Pass Output Data Frame
+            outDFSurvey = []
+
+            logMsg = f"Success ETL_SNPLPORE.py - proces_Survey."
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.info(logMsg)
+
+            return outDFSurvey
+
+        except Exception as e:
+
+            logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - proces_Survey: {e}'
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.critical(logMsg, exc_info=True)
+            traceback.print_exc(file=sys.stdout)
+
+        def proces_Observations(outDFDic, etlInstance, dmInstance, outDFSurvey):
+            try:
+                """
+                ETL routine for the 'SNPL Observation' form - table 'SNPL_Observations'.
+                The major of information on this form is pushed to the following tables:
+                tbl_SNPL_Observations
+                xxxx
+
+                :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+                :param etlInstance: ETL processing instance
+                :param dmInstance: Data Management instance
+                :param outDFSurvey: Data frame output from the proces_Survey method.  Used is workflow processing.
+
+                :return:outDFObs: Data Frame with the exported/imported Observation data, to be used as needed.  
+                """
+
+                # Pass Output Data Frame
+                outDFSurvey = []
+
+                logMsg = f"Success ETL_SNPLPORE.py - process_Observations."
+                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+                logging.info(logMsg)
+
+                return outDFObs
+
+            except Exception as e:
+
+                logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_Observations: {e}'
+                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+                logging.critical(logMsg, exc_info=True)
+                traceback.print_exc(file=sys.stdout)
+
+        def proces_Bands(outDFDic, etlInstance, dmInstance, outDFSurvey, outDFObs):
+
+            """
+            ETL routine for the 'SNPL Bands Sub Form' form - table 'tbl_SNPL_Banded'.
+            The major of information on this form is pushed to the following tables:
+            tbl_SNPL_Banded
+
+
+            :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+            :param etlInstance: ETL processing instance
+            :param dmInstance: Data Management instance
+            :param outDFSurvey: Data frame output from the proces_Survey method.
+            :param outDFObs: Data frame output from the process_Observations method.
+
+            :return:outDFBands: Data Frame with the exported/imported data, to be used as needed.
+            """
+
+            try:
+                # Pass Output Data Frame
+                outDFSurvey = []
+
+                logMsg = f"Success ETL_SNPLPORE.py - process_Bands."
+                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+                logging.info(logMsg)
+
+                return outDFBands
+
+            except Exception as e:
+
+                logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_Bands: {e}'
+                dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+                logging.critical(logMsg, exc_info=True)
+                traceback.print_exc(file=sys.stdout)
+
+    def proces_Predator(outDFDic, etlInstance, dmInstance, outDFSurvey):
+
+        """
+        ETL routine for the 'Predator Observations' form - table 'PredatorObservations_2'.
+        The major of information on this form is pushed to the following tables:
+        tbl_Predator_Survey
+
+
+        :param outDFDic - Dictionary with all imported dataframes from the imported feature layer
+        :param etlInstance: ETL processing instance
+        :param dmInstance: Data Management instance
+        :param outDFSurvey: Data frame output from the proces_Survey method.
+
+        :return:outDFPredator: Data Frame with the exported/imported data
+        """
+
+        try:
+            # Pass Output Data Frame
+            outDFSurvey = []
+
+            logMsg = f"Success ETL_SNPLPORE.py - process_Predators."
+            dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
+            logging.info(logMsg)
+
+            return outDFPredator
+
+        except Exception as e:
+
+            logMsg = f'WARNING ERROR  - ETL_SNPLPORE.py - process_Predators: {e}'
             dm.generalDMClass.messageLogFile(dmInstance, logMsg=logMsg)
             logging.critical(logMsg, exc_info=True)
             traceback.print_exc(file=sys.stdout)
