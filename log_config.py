a60849c6a882edf63306da8993dbb25c3ab74d54
krsherrill <kirk_Sherrill@nps.gov>
2025-03-26 17:10:16 -0600

    Update log_config.py
    Updated Logger File code to match other SFAN Logger file setup logic.

--- log_config.py
+++ log_config.py
@@ -6,13 +6,12 @@ log file configuration script
 
 """
 def setup_logging():
-    # Create Unique Log File Each Run
-    log_filename = f"ETLProcessingLog_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
-    # Configure logging
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s - %(levelname)s - %(message)s',
-                        filename=log_filename,  # Log to a file
-                        filemode='w')        # Append to the log file
+                        handlers=[
+                            logging.FileHandler("ScriptProcessingLog.log", mode="w"),  # File output A is append
+                            logging.StreamHandler()  # Console output
+                        ])
 
 # Call the setup function to configure logging
 setup_logging()
