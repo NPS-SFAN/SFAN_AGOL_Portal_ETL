377ef1b56597efaa0e93266bb416a3cbe5d0e4a9
krsherrill <kirk_Sherrill@nps.gov>
2025-05-29 17:01:38 -0600

    Update ETL.py
    Added Smolts processing; other minor syntax updates.

--- ETL.py
+++ ETL.py
@@ -9,6 +9,7 @@ import ArcGIS_API as agl
 import logging
 import ETL_SNPLPORE as SNPLP
 import ETL_Salmonids_Electro as SEfish
+import ETL_Salmonids_Smolts as SSmolt
 import ETL_PCM_LocationsManualParking as PCMLOC
 import ETL_PINN_Elephant as PElephant
 
@@ -63,23 +64,29 @@ class etlInstance:
 
             # Create the protocol specific ETL instance
             # ETL Routine Snowy Plover PORE
-            if etlInstance.protocol == 'SNPLPORE':
+            if etlInstance.protocol.lower() == 'snplpore':
                 # Pull the Feature Layer for the defined return as dataframe(s) in list variable outDFList
                 outDFDic = agl.generalArcGIS.processFeatureLayer(generalArcGIS, etlInstance, dmInstance)
                 outETL = SNPLP.etl_SNPLPORE.process_ETLSNPLPORE(outDFDic, etlInstance, dmInstance)
 
             # ETL Routine Salmonids Electrofishing
-            elif etlInstance.protocol == 'Salmonids-EFish':
+            elif etlInstance.protocol.lower() == 'salmonids-efish':
                 # Pull the Feature Layer for the defined return as dataframe(s) in list variable outDFList
                 outDFDic = agl.generalArcGIS.processFeatureLayer(generalArcGIS, etlInstance, dmInstance)
                 outETL = SEfish.etl_SalmonidsElectro.process_ETLElectro(outDFDic, etlInstance, dmInstance)
 
+            # ETL Routine Salmonids Smolts
+            elif etlInstance.protocol.lower() == 'salmonids-smolts':
+                outDFDic = agl.generalArcGIS.processFeatureLayer(generalArcGIS, etlInstance, dmInstance)
+                outETL = SSmolt.etl_SalmonidsSmolts.process_ETLSmolts(outDFDic, etlInstance, dmInstance)
+
             # ETL Routine Pinnipeds Elephant Seal
-            elif etlInstance.protocol == 'PINN-Elephant':
+            elif etlInstance.protocol.lower() == 'pinn-elephant':
                 outDFDic = agl.generalArcGIS.processFeatureLayer(generalArcGIS, etlInstance, dmInstance)
                 outETL = PElephant.etl_PINNElephant.process_PINNElephant(outDFDic, etlInstance, dmInstance)
 
-            elif etlInstance.protocol == 'PCM-LocationsManual':
+            # PCM Plot Locations Manual
+            elif etlInstance.protocol.lower() == 'pcm-locationsmanual':
                 outETL = PCMLOC.etl_PCMLocations.process_PCMLocManual(etlInstance, dmInstance, generalArcGIS)
 
             else:
