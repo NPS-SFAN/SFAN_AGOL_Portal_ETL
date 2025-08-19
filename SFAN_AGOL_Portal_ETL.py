4933d413a05a9a58e0d604deb84a3d908f382827
krsherrill <kirk_Sherrill@nps.gov>
2025-08-19 10:36:17 -0600

    Update SFAN_AGOL_Portal_ETL.py

--- SFAN_AGOL_Portal_ETL.py
+++ SFAN_AGOL_Portal_ETL.py
@@ -47,7 +47,7 @@ logger = logging.getLogger(__name__)
 # Protocol/Item Being Processes
 protocol = 'PINN-Elephant'   # (SNPLPORE|Salmonids-EFish|Salmonids-Smolts|PCM-LocationsManual|PINN-Elephant)
 # Access Backend Database for the protocol
-inDBBE = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\VitalSigns\Pinnipeds\Data\Database\PinnipedBE_20250814 - Testing.accdb'
+inDBBE = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\VitalSigns\Pinnipeds\Data\Database\PinnipedBE_20250819.accdb'
 inDBFE = r'C:\Users\KSherrill\OneDrive - DOI\SFAN\VitalSigns\Pinnipeds\Data\Database\PinnipedFE_20250806.accdb'
 
 # Year Being Processed
